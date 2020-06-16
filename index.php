61959e68c600ece2c363f3d18572f4bf9b875100
Viktoriia172 <vderevliuk@gmail.com>
2020-06-16 19:15:43 +0300

    change main page

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Ocean</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="cssload-container">
+            <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        <!-- Navbar Area -->
+        <div class="palatin-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="palatinNav">
+
+                        <!-- Nav brand -->
+                        <a href="index.php" class="nav-brand"><img src="img/core-img/trip.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="active"><a href="index.php">Home</a></li>
+                                    <li><a href="find_cruise.php">Find a Cruise</a></li>
+                                    <li><a href="#contact">Contact us</a></li>
+                                    <li><a href="request_info.php">Request information</a></li>   
+                                    <li><a href="login.php"><img src="https://img.icons8.com/material-outlined/24/000000/writer-male.png"/></a></li>                                 
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <section class="hero-area">
+        <div class="hero-slides owl-carousel">
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <!-- Slide Img -->
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/Palawan-Philippines.jpeg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <!-- Slide Content -->
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">TRUE LUXURY CRUISING</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">On board the world's most luxurious fleet, relax in the comforts of your spacious and immaculate suite, savor the flavors of the exquisite cuisine in our specialty restaurants and make the most of every moment, without a care in the world.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <!-- Slide Img -->
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/bg-5.jpg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <!-- Slide Content -->
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">IMMERSIVE EXPLORATION</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">Experience more than ever before as you sail the world with Regent Seven Seas Cruises. Take in the beauty of each destination with new ways to explore alongside our hallmark inclusion of FREE Unlimited Shore Excursions across more than 450 ports of call throughout the globe.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <!-- Slide Img -->
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/bg-3.jpg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <!-- Slide Content -->
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">RETURN WITH OCEAN</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">Return with Ocean to travel the world in total luxury, enjoying the security and flexibility of Ocean Reassurance, plus a Reduced $1,000 Shipboard Credit on all voyages embarking on or before 31 December 2022.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="about-us-area">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-12 col-lg-6">
+                    <div class="about-text text-center mb-100">
+                        <div class="section-heading text-center">
+                            <div class="line-"></div>
+                            <h2>A place to remember</h2>
+                        </div>
+                        <p>We live in an amazing world, filled with giant glaciers, staggering mountains and plains dotted with wild animals. We may not be able to travel right now, but as soon as we can safely do so these are the best places visit. Move these 50 awe-inspiring landscapes – from America to Europe – to the top of your travel list.</p>
+                        <div class="about-key-text">
+                            <h6><span class="fa fa-check"></span> Choose the best</h6>
+                            <h6><span class="fa fa-check"></span> Fallow Your dream with us!</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="about-thumbnail homepage mb-100">
+                        <!-- First Img -->
+                        <div class="first-img wow fadeInUp" data-wow-delay="100ms">
+                            <img src="img/bg-img/94193f53350dbf13a53ab10aef1adfda.jpeg" alt="">
+                        </div>
+                        <!-- Second Img -->
+                        <div class="second-img wow fadeInUp" data-wow-delay="300ms">
+                            <img src="img/bg-img/berlin-gay-penguins-adopt-abandoned-egg-01.jpg" alt="">
+                        </div>
+                        <!-- Third Img-->
+                        <div class="third-img wow fadeInUp" data-wow-delay="500ms">
+                            <img src="img/bg-img/11111111.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- ##### Pool Area Start ##### -->
+    <section class="pool-area section-padding-100 bg-img bg-fixed" style="background-image: url(img/bg-img/csm_EUROPA-2-Yachtclub_9eda058189.jpg);">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-12 col-lg-7">
+                    <div class="pool-content text-center wow fadeInUp" data-wow-delay="300ms">
+                        <div class="section-heading text-center white">
+                            <div class="line-"></div>
+                            <h2>LIFE ON BOARD</h2>
+                            <p>All guests are pampered equally aboard our ships. We are the only cruise line in the world to include butler service in every suite, in every category. A staff-to-guest ratio of nearly one to one ensures that your every wish is fulfilled with earnest precision, from the concierge who can customise your voyage to the butler who serves your breakfast in suite.</p>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 col-sm-4">
+                                <div class="pool-feature">
+                                    <i class="icon-cocktail-1"></i>
+                                    <p>5 Bars and 6 Restaurants</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-4">
+                                <div class="pool-feature">
+                                    <i class="icon-swimming-pool"></i>
+                                    <p>Pool area on board</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-4">
+                                <div class="pool-feature">
+                                    <i class="icon-beach"></i>
+                                    <p>Sunbeds</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Pool Area End ##### -->
+
+
+    <section class="rooms-area section-padding-100-0">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-6">
+                    <div class="section-heading text-center">
+                        <div class="line-"></div>
+                        <h2>SPECIAL OFFERS</h2>
+                        <p>Return with OCEAN to travel the world in total luxury, enjoying the security and flexibility of Ocean Reassurance, plus a Reduced 10% Deposit* and $1,000 Shipboard Credit on all voyages embarking on or before 31 December 2022.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-rooms-area wow fadeInUp" data-wow-delay="100ms">
+                        <!-- Thumbnail -->
+                        <div class="bg-thumbnail bg-img" style="background-image: url(img/bg-img/122.jpg);"></div>
+                        <!-- Price -->
+                        <p class="price-from">All inclusive</p>
+                        <!-- Text -->
+                        <div class="rooms-text">
+                            <div class="line"></div>
+                            <h4>Deluxe Service</h4>
+                            <p>If you want wonderful service, great food, and a smaller ship than there is only one Cruise Line OCEAN!</p>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-rooms-area wow fadeInUp" data-wow-delay="300ms">
+                        <!-- Thumbnail -->
+                        <div class="bg-thumbnail bg-img" style="background-image: url(img/bg-img/French-Polynesia-9.jpg);"></div>
+                        <!-- Price -->
+                        <p class="price-from">Free excursion</p>
+                        <!-- Text -->
+                        <div class="rooms-text">
+                            <div class="line"></div>
+                            <h4>luxury Voyages</h4>
+                            <p>Discover our exclusive collection of luxury voyages with outstanding savings.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Best Rooms -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-rooms-area wow fadeInUp" data-wow-delay="500ms">
+                        <!-- Thumbnail -->
+                        <div class="bg-thumbnail bg-img" style="background-image: url(img/bg-img/Silversea-Cruise-2.jpg);"></div>
+                        <!-- Price -->
+                        <p class="price-from">Best view</p>
+                        <!-- Rooms Text -->
+                        <div class="rooms-text">
+                            <div class="line"></div>
+                            <h4>BEST ROOMS</h4>
+                            <p>Always beautiful view from your window?makes you feel happy all day.Choose what you like</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- ##### Contact Area Start ##### -->
+    <section class="contact-area d-flex flex-wrap align-items-center">
+        <div class="home-map-area">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325518.6878344313!2d30.252510061496128!3d50.401699044222816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce88e35e395f%3A0xc63774cf5da5a434!2z0KHQvtCx0L7RgCDQodC-0YTRltGPINCa0LjRl9Cy0YHRjNC60LA!5e0!3m2!1sru!2sua!4v1592307908807!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+        <!-- Contact Info -->
+        <div id="contact" class="contact-info">
+            <div class="section-heading wow fadeInUp" data-wow-delay="100ms">
+                <div class="line-"></div>
+                <h2>Contact Info</h2>
+                <p>If you have any further questions, please do not hesitate to contact us.</p>
+            </div>
+            <h4 class="wow fadeInUp" data-wow-delay="300ms">Kyiv, Ukrain, Volodimirska str.24 <br><br><p><h5>HOURS OF OPERATION
+                    WEEKDAYS:<h5/></p>
+                    <p>9:30am - 6:00 pm</p><p>
+                    SATURDAY:
+                    Closed</p>
+                    <p>SUNDAY: Closed</p></h4><br><br>
+            <h5 class="wow fadeInUp" data-wow-delay="400ms">+38 073 47 13 708</h5>
+            <h5 class="wow fadeInUp" data-wow-delay="500ms">mail@gmail.com</h5>
+            <!-- Social Info -->
+            <div class="social-info mt-50 wow fadeInUp" data-wow-delay="600ms">
+                <a href="https://www.pinterest.com/carnivalcruise/"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/ilovecruiseships/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/princesscruises"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://dribbble.com/tags/cruise_ship"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Contact Area End ##### -->
+
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-lg-5">
+                    <div class="footer-widget-area mt-50">
+                        <a href="#" class="d-block mb-5"><img src="img/core-img/trip.png" alt=""></a>
+                        <p>Enjoy special Single Supplements starting at only 10% above the advertised per person fare on select voyages. Embark on a solo journey to discover the awe-inspiring wonders of America to the history in Northern Europe to the sun-kissed beaches of the Mediterranean to the iconic landmarks of Canada … the choice is yours. </p>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Area -->
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="footer-widget-area mt-50">
+                        <h6 class="widget-title mb-5">Find us on the map</h6>
+                        <img src="img/bg-img/footer-map.png" alt="">
+                    </div>
+                </div>
+
+
+                <!-- Copywrite Text -->
+                <div class="col-12">
+                    <div class="copywrite-text mt-30">
+                        <p><a href="#">&copy;<script>document.write(new Date().getFullYear());</script> Team №1/module№3 | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> and help of <a href="https://colorlib.com" target="_blank">EduCoin curses</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area End ##### -->
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
