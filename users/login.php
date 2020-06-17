9517dc7475c5525fe14887f9fb20d7f9303e347c
Yuliia_Sh <yu.expert@gmail.com>
2020-06-17 23:26:21 +0300

    sql for login

--- users/login.php
+++ users/login.php
@@ -1,11 +1,46 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
+include $_SERVER['DOCUMENT_ROOT'].'/configs/db.php';
+
+/*
++ 1. Сделать форму авторизации
++ 2. Сделать отправку формы
++ 3. Сделать проверку на существование юзера в БД
++ 4. Сделать проверку на прохождение верификации
+*/
+
+
+
+// Если существует ПОСТ запрос (при нажатии на кнопку SUBMIT)
+// и если поля почта и пароль не пусты
+if (isset($_POST) && $_SERVER["REQUEST_METHOD"]=="POST" && $_POST['usmail'] !="" && $_POST['pass'] !="") {
+	$password = md5($_POST['pass']);
+	
+	// Авторизация - если логин и пароль совпали с БД, то авторизация успешна
+	$sql = "SELECT * FROM users WHERE email='" . $_POST['usmail'] . "' AND password='$password'";
+	$result = $conn->query($sql);
+	$user = mysqli_fetch_assoc($result);
+	// Если пользователь верефецирован ($user['verifided'] == 1)
+	if($result->num_rows > 0 && $user['verifided'] == 1) {
+		
+			setcookie("user_id", $user["id"], time() + 60*60, "/");
+			// header("Location: /index.php");
+			echo "NAIDEN";
+	} else if($result->num_rows > 0 && $user['verifided'] == 0){ // Если пользователь НЕ верефецирован
+		echo "<script>alert(\"You need verification!\");</script>";
+		header("Location: /users/verification.php");
+	} else	{ // Если пользователь ввел НЕ верно логин или пароль
+		// echo "<h3>Login or password is incorrect.</h3>";
+		echo "<script>alert(\"Login or password is incorrect.\");</script>";
+	}
+}
+
 ?>
 
     <!-- ##### Breadcumb Area Start ##### -->
     <section class="breadcumb-area bg-img d-flex align-items-center justify-content-center" style=" background-image: url(../img/bg-img/bg-8.jpg);">
         <div class="bradcumbContent">
-            <h2>LOG IN</h2>
+            <h2>MY ACCOUNT</h2>
         </div>
     </section>
     <!-- ##### Breadcumb Area End ##### -->
@@ -17,7 +52,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
                 <div class="col-12">
                     <div class="section-heading">
                         <div class="line-"></div>
-                        <h2>Log in</h2>
+                        <h2>Sign in</h2>
                     </div>
                 </div>
             </div>
@@ -25,17 +60,21 @@ include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
             <div class="row">
                 <div class="col-12">
                     <!-- Contact Form -->
-                    <form action="#" method="post">
+                    <form method="POST">
                         <div class="row">
                             <div class="col-lg-6">
-                                <input type="email" class="form-control" name="email" placeholder="E-mail">
+                                <input type="email" class="form-control" name="usmail" placeholder="Email Address">
                             </div>
                             <div class="col-lg-6">
-                                <input type="password" class="form-control" name="password" placeholder="Password">
+                                <input type="password" class="form-control" name="pass" placeholder="Password">
                             </div>
                             <div class="col-12">
-                                <button type="submit" class="btn palatin-btn mt-50">Log in</button>
+                                <button type="submit" class="btn palatin-btn mt-50">SUBMIT</button>
                             </div>
+                            <div class="col-md-12 mt-3">
+                            	Don't have an account?
+								<a href="/users/registr.php" class="no-text-decoration blue-link"><b style="color: #51748b;">CREATE AN ACCOUNT.</b></a>
+							</div>
                         </div>
                     </form>
                 </div>
