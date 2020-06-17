0e0c881f957e91ee415f88dafee8de1ba7ddb674
Yuliia_Sh <yu.expert@gmail.com>
2020-06-17 17:28:08 +0300

    sql for registration

--- users/registr.php
+++ users/registr.php
@@ -1,5 +1,61 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
+include $_SERVER['DOCUMENT_ROOT'].'/configs/db.php';
+
+/*
+1. Сделать форму регистрации +
+2. Сделать отправку формы
+3. Сделать отпрвку письма со ссылкой на подтверждение регистрации
+4. Сделать страницу с подтверждением регистрации
+*/
+
+// если существует ГЕТ запрос с кодом
+if( isset($_GET['u_code']) ) {
+	// выбираем из базы данных поля, где код в ГЕТ запросе совпадает с кодом из БД
+	$sql = "SELECT * FROM users WHERE confirm_mail='" . $_GET['u_code'] . "' ";
+	$result = $conn->query($sql);
+	// и если найдено совпадение, меняем в БД verifided с 0 (который стоит по умолчанию) на 1
+	if($result->num_rows > 0) {
+		$user = mysqli_fetch_assoc($result);
+		$sql = "UPDATE users SET verifided = '1', confirm_code = '' WHERE id =" . $user['id'];
+		if($conn->query($sql)) {
+			// echo "User verifided!";
+			echo "<script>alert(\"User verifided!\");</script>";
+		}
+	}
+}
+
+
+// Если существует ПОСТ запрос (при нажатии на кнопку Зарегистрироваться)
+if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST" 
+	// и если поля Имя, телефон, почта и пароль не пусты
+	&& $_POST['usname'] !="" && $_POST['usphone'] !="" && $_POST['usmail'] !="" && $_POST['pass'] !="") {
+	// для безопасности взлома кодируем пароль, чтоб в базе данных он был не доступен
+	$password = md5($_POST['pass']);
+	// Запускаем функцию генерации ссылки со случайной строкой для верификации почты
+	$u_code = generateRandomString(20);
+	
+	// Регистрация
+	$sql = "INSERT INTO users (name, phone, email, password, confirm_code) VALUES ('" . $_POST['usname'] . "', '" . $_POST['usphone'] . "', '" . $_POST['usmail'] . "', '" . $password . "', '$u_code')";
+
+	if($conn->query($sql)) {
+		echo "<h3>Successfully registered.</h3>";
+		// $link = "<a href='http://ocean.local/registr.php?u_code=$u_code'>Confirm</a>";
+		// mail($_POST['usmail'], 'Registration', $link);
+	} 
+} 
+
+
+// Функция генерации случайного кода (строки) для верификации mail
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 ?>
 
     <!-- ##### Breadcumb Area Start ##### -->
@@ -26,19 +82,19 @@ include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
             <div class="row">
                 <div class="col-12">
                     <!-- Contact Form -->
-                    <form action="#" method="post">
+                    <form method="POST">
                         <div class="row">
                         	<div class="col-lg-6">
-                                <input type="text" class="form-control" name="text" placeholder="Your Name">
+                                <input type="text" class="form-control" name="usname" placeholder="Your Name">
                             </div>
                             <div class="col-lg-6">
-                                <input type="phone" class="form-control" name="phone" placeholder="Phone Number">
+                                <input type="phone" class="form-control" name="usphone" placeholder="Phone Number">
                             </div>
                             <div class="col-lg-6">
-                                <input type="email" class="form-control" name="email" placeholder="E-mail">
+                                <input type="email" class="form-control" name="usmail" placeholder="E-mail">
                             </div>
                             <div class="col-lg-6">
-                                <input type="password" class="form-control" name="password" placeholder="Password">
+                                <input type="password" class="form-control" name="pass" placeholder="Password">
                             </div>
                             <div class="col-12">
                                 <button type="submit" class="btn palatin-btn mt-50">Registration</button>
