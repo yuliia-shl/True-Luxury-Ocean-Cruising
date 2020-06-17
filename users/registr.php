5c6ff645e0c561c5cdcaebe58c80d5af067be436
Yuliia_Sh <yu.expert@gmail.com>
2020-06-17 16:47:52 +0300

    include header&footer

--- users/registr.php
+++ users/registr.php
@@ -1,81 +1,9 @@
-<?php
+<?php 
+include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-    <!-- Title -->
-    <title>Ocean</title>
-
-    <!-- Favicon -->
-    <link rel="icon" href="../img/core-img/favicon.ico">
-
-    <!-- Core Stylesheet -->
-    <link rel="stylesheet" href="../style.css">
-
-</head>
-
-<body>
-    <!-- Preloader -->
-    <div class="preloader d-flex align-items-center justify-content-center">
-        <div class="cssload-container">
-            <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
-        </div>
-    </div>
-
-    <!-- ##### Header Area Start ##### -->
-    <header class="header-area">
-        <!-- Navbar Area -->
-        <div class="palatin-main-menu">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
-                    <!-- Menu -->
-                    <nav class="classy-navbar justify-content-between" id="palatinNav">
-
-                        <!-- Nav brand -->
-                        <a href="/index.php" class="nav-brand"><img src="../img/core-img/trip.png" alt=""></a>
-
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-
-                        <!-- Menu -->
-                        <div class="classy-menu">
-
-                            <!-- close btn -->
-                            <div class="classycloseIcon">
-                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                            </div>
-
-                            <!-- Nav Start -->
-                            <div class="classynav">
-                                <ul>
-                                    <li class="active"><a href="/index.php">Home</a></li>
-                                    <li><a href="find_cruise.php">Find a Cruise</a></li>
-                                    <li><a href="#contact">Contact us</a></li>
-                                    <li><a href="request_info.php">Request information</a></li>   
-                                    <li><a href="login.php"><img src="https://img.icons8.com/material-outlined/24/000000/writer-male.png"/></a></li>                                 
-                                </ul>
-                            </div>
-                            <!-- Nav End -->
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- ##### Header Area End ##### -->
-
     <!-- ##### Breadcumb Area Start ##### -->
-    <section class="breadcumb-area bg-img d-flex align-items-center justify-content-center" style=" background-image: url(../img/bg-img/bg-8.jpg);">
+    <section class="breadcumb-area bg-img d-flex align-items-center justify-content-center" style=" background-image: url(/img/bg-img/bg-8.jpg);">
         <div class="bradcumbContent">
             <h2>Registration</h2>
         </div>
@@ -83,7 +11,7 @@
     <!-- ##### Breadcumb Area End ##### -->
 
 
-    <!-- ##### Contact Form Area Start ##### -->
+    <!-- ##### Registration Form Area Start ##### -->
     <section class="contact-form-area mb-100">
         <div class="container">
             <div class="row">
@@ -121,93 +49,9 @@
             </div>
         </div>
     </section>
-    <!-- ##### Contact Form Area End ##### -->
+    <!-- ##### Registration Form Area End ##### -->
 
-    <!-- ##### Header Area Start ##### -->
-    <header class="header-area">
-        <!-- Navbar Area -->
-        <div class="palatin-main-menu">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
-                    <!-- Menu -->
-                    <nav class="classy-navbar justify-content-between" id="palatinNav">
 
-                        <!-- Nav brand -->
-                        <a href="/index.php" class="nav-brand"><img src="../img/core-img/trip.png" alt=""></a>
-
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-
-                        <!-- Menu -->
-                        <div class="classy-menu">
-
-                            <!-- close btn -->
-                            <div class="classycloseIcon">
-                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                            </div>
-
-                            <!-- Nav Start -->
-                            <div class="classynav">
-                                <ul>
-                                    <li class="active"><a href="/index.php">Home</a></li>
-                                    <li><a href="find_cruise.php">Find a Cruise</a></li>
-                                    <li><a href="#contact">Contact us</a></li>
-                                    <li><a href="request_info.php">Request information</a></li>   
-                                    <li><a href="login.php"><img src="https://img.icons8.com/material-outlined/24/000000/writer-male.png"/></a></li>                                 
-                                </ul>
-                            </div>
-                            <!-- Nav End -->
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
- <!-- ##### Footer Area Start ##### -->
-    <footer class="footer-area">
-        <div class="container">
-            <div class="row">
-
-                <!-- Footer Widget Area -->
-                <div class="col-12 col-lg-5">
-                    <div class="footer-widget-area mt-50">
-                        <a href="#" class="d-block mb-5"><img src="../img/core-img/trip.png" alt=""></a>
-                        <p>Enjoy special Single Supplements starting at only 10% above the advertised per person fare on select voyages. Embark on a solo journey to discover the awe-inspiring wonders of America to the history in Northern Europe to the sun-kissed beaches of the Mediterranean to the iconic landmarks of Canada … the choice is yours. </p>
-                    </div>
-                </div>
-
-                <!-- Footer Widget Area -->
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="footer-widget-area mt-50">
-                        <h6 class="widget-title mb-5">Find us on the map</h6>
-                        <img src="/../img/bg-img/footer-map.png" alt="">
-                    </div>
-                </div>
-
-                <!-- Copywrite Text -->
-                <div class="col-12">
-                    <div class="copywrite-text mt-30">
-                        <p><a href="#">&copy;<script>document.write(new Date().getFullYear());</script> Team №1/module№3 | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> and help of <a href="https://colorlib.com" target="_blank">EduCoin curses</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- ##### Footer Area End ##### -->
-
-    <!-- ##### All Javascript Script ##### -->
-    <!-- jQuery-2.2.4 js -->
-    <script src="../js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <!-- <script src="../js/bootstrap/popper.min.js"></script> -->
-    <!-- Bootstrap js -->
-    <!-- <script src="../js/bootstrap/bootstrap.min.js"></script> -->
-    <!-- All Plugins js -->
-    <script src="../js/plugins/plugins.js"></script>
-    <!-- Active js -->
-    <script src="../js/active.js"></script>
-</body>
-
-</html>
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/parts/footer.php';
+?>
