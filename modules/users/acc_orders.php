8ce700510960efc469429b181a6e15277cfef05c
Yuliia_Sh <yu.expert@gmail.com>
2020-06-20 18:00:40 +0300

    added file acc_orders, did functional for view orders

---
+++ modules/users/acc_orders.php
@@ -0,0 +1,53 @@
+<!-- ================================
+Файл Вывода заказов в Личном кабинете
+==================================-->
+<?php
+
+// Если пользователь авторизован
+if(isset($_COOKIE['login'])) {
+	// вытягиваем из БД users где user_id совпадают с куки
+	// вытягиваем из БД orders где user_id совпадают с куки
+    $sql_us = "SELECT * FROM users WHERE id =" . $_COOKIE["login"];
+    $result_us = $conn->query($sql_us);
+    $users = mysqli_fetch_assoc($result_us);
+
+
+	// вытягиваем из БД orders где user_id совпадают с куки
+	// вытягиваем из БД user с таким же айди, как user_id в orders
+	$sql_ord = "SELECT * FROM orders WHERE user_id=" . $users ['id'];
+    $result_ord = $conn->query($sql_ord);
+    $row_ord = mysqli_fetch_assoc($result_ord);
+
+
+
+	// Если масив заказа существует
+    if (isset($row_ord['cruis_list'])) {
+        // помещаем в переменную массив из БД (преобразованный из json формата) 
+        $cruise_order = json_decode($row_ord['cruis_list'], true);
+
+        // подсчитываем сколько в массиве обьектов и сколько значений у обьектов
+        for ($i = 0; $i < count($cruise_order['basket']); $i++) {
+            // Вытягиваем инфо из 3х таблиц
+            $sql_cru = "SELECT cruises.*, destinations.departure, destinations.arrival, categories.title
+	            FROM cruises, destinations, categories
+	            WHERE cruises.id= '" . $cruise_order['basket'][$i]['cruis_id'] . "'  AND cruises.destinations_id = destinations.id AND destinations.categori_id = categories.id";
+	        // Получаем результат
+            $result_cru = $conn->query($sql_cru);
+            $cruise = mysqli_fetch_assoc($result_cru);
+          ?>
+    	  <tbody>
+			<td class="center"><?php echo $cruise['data']; ?></td>
+            <td class="left"><?php echo $cruise['title']; ?></td>
+            <td class="right"><?php echo $cruise_order['basket'][$i]['days']; ?></td>
+            <td class="center"><?php echo number_format ($cruise['price'] * $cruise_order['basket'][$i]['days'],2, ',' , ' '); ?></td>
+            <td class="left"><?php echo $cruise['title']; ?></td>
+            <td class="left"><?php echo $cruise['departure']; ?> TO <?php echo $cruise['arrival']; ?></td>
+          </tbody>
+			<!--Table body-->
+          <?php
+        }
+    } else {
+    	echo "<p>You currently have no booked cruises. Please use the <a style=\"color: blue\" href=\"/cruises.php\"> cruise finder </a> to find cruises you are interested in and use the Book Cruise link to add them to your order list.</p>";
+    }
+}
+?>
--- modules/users/account.php
+++ modules/users/account.php
@@ -1,7 +1,6 @@
 <!-- ========================
 Файл Личного кабинета клиента
 ==========================-->
-
 <?php 
 $page = "login";
 include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
@@ -10,25 +9,17 @@ include $_SERVER['DOCUMENT_ROOT'].'/configs/db.php';
 /*
 + 1. Сделать страницу кабинета
 - 2. Сделать кнопки - функционал (Изменить инфо, Посмотреть заказы)
-- 3. Сделать страницу под кнопку Посмотреть заказы - вывести заказы с orders
++ 3. Сделать страницу под кнопку Посмотреть заказы - вывести заказы с orders, destinations, categories
 - 4. Сделать страницу под кнопку Изменить инфо:
 	4.1. Сделать форму изменения данных
 	4.2. Сделать отправку формы
 */
-
-if(isset($_COOKIE['login'])) {
-	$sql = "SELECT * FROM users WHERE id=" . $_COOKIE["login"];
-	$result = mysqli_query($conn, $sql);
-	$user = mysqli_fetch_assoc($result);
-
-}
-
 ?>
 
 <!-- ##### Breadcumb Area Start ##### -->
 <section class="breadcumb-area bg-img d-flex align-items-center justify-content-center" style=" background-image: url(/img/bg-img/4.png);">
     <div class="bradcumbContent">
-        <h2>MY ACCOUNT</h2>
+        <h2>MY PROFILE</h2>
     </div>
 </section>
 <!-- ##### Breadcumb Area End ##### -->
@@ -36,115 +27,69 @@ if(isset($_COOKIE['login'])) {
 
 <!-- ##### Registration Form Area Start ##### -->
 <section class="contact-form-area mb-100">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="section-heading">
-                    <div class="line-"></div>
-                    <h2>Welcome, <?php echo $user['name']; ?></h2>
-                </div>
-                <div class="palatin-buttons-area mb-100">
-                        <a href="#" class="btn palatin-btn btn-3 m-2">MY CRUISES</a>
-                        <a href="#" class="btn palatin-btn btn-3 m-2">View profile</a>
-                        <a href="#" class="btn palatin-btn btn-3 m-2">smth</a>
-                </div>
+  <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="section-heading">
+                <div class="line-"></div>
+                <h2>Welcome, <?php echo $user['name']; ?></h2>
             </div>
-        
-    		<!-- ##### Accordians ##### -->
-            <div class="col-12 col-lg-12">
-                <div class="accordions mb-3" id="accordion" role="tablist" aria-multiselectable="true">
-                    <!-- single accordian area -->
-                    <div class="panel single-accordion">
-                        <h6>
-                        	<a role="button" class="" aria-expanded="true" aria-controls="collapseOne" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">MY CRUISES
-                                <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
-                                <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
-                                </a>
-                        </h6>
-                        <div id="collapseOne" class="accordion-content collapse show">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris sceleri sque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus. Lorem ipsum dolor sit amet.</p>
-                            <div class="table-responsive">
-<!-- 						<div class="btn btn-success mb-2">
-							<a href="/admin/modules/categories/add.php">ADD</a>
-						</div>  -->
+<!--             <div class="palatin-buttons-area mb-100">
+                    <a href="#" class="btn palatin-btn btn-3 m-2">MY CRUISES</a>
+                    <a href="#" class="btn palatin-btn btn-3 m-2">View profile</a>
+                    <a href="#" class="btn palatin-btn btn-3 m-2">smth</a>
+            </div> -->
+        </div>
+    
+		<!-- ##### Accordians ##### -->
+      <div class="col-12 col-lg-12">
+        <div class="accordions mb-3" id="accordion" role="tablist" aria-multiselectable="true">
+            <!-- single accordian area -->
+            <div class="panel single-accordion">
+                <h6>
+                	<a role="button" class="" aria-expanded="true" aria-controls="collapseOne" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">BOOKED CRUISES
+                        <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                        <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
+                        </a>
+                </h6>
+              <div id="collapseOne" class="accordion-content collapse show">
+                    <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem ma.</p> -->
+                <div class="table-responsive">
+	                <table class="table">
+	                    <thead class=" text-primary">
+	                      <th>Departs date</th>
+					      <th>Title</th>
+					      <th>Days</th>
+					      <th>Price,$</th>
+					      <th>Category</th>
+	                      <th>Destination</th>
+	                    </thead>
 
-		                <table class="table">
-		                    <thead class=" text-primary">
-		                      <th>Date</th>
-						      <th>Title</th>
-						      <th>Days</th>
-						      <th>Price</th>
-						      <th>Category</th>
-		                      <th>Destination</th>
-		                    </thead>
-		                    <!--Table body-->
-						    <tbody>
-						      <?php
-				            	// Делаем вывод пользователей 
-								$sql = "SELECT * FROM users";
-								$result = $conn->query($sql);
-								// пока есть кол-во результатов (пользователей)
-								while ($row = mysqli_fetch_assoc($result)) {
-				                  ?>
-									<tr>
-				                        <td><?php echo $row ['id'] ?></td>
-				                        <td><?php echo $row ['name'] ?></td>
-				                        <td><?php echo $row ['phone'] ?></td>
-				                        <td><?php echo $row ['email'] ?></td>
-				                        <td><?php echo $row ['id'] ?></td>
-				                        <td><?php echo $row ['id'] ?></td>
-				                        <?php
-							              if ($row['verifided'] == "1") {
-							              	echo "<td>YES</td>";  
-							              } else {
-							              	echo "<td>NO</td>";
-							              }
-							              ?>
-				                    </tr>
-				                  <?php
-				                }
-				              ?>
-						    </tbody>
-						    <!--Table body-->
-			             </table>
-		                </div>
-		              </div>
-	                </div>
+	                    <!--Table body-->
+	                    <?php
+	                    include $_SERVER['DOCUMENT_ROOT'].'/modules/users/acc_orders.php';
+			            ?>
+
+		            </table>
                 </div>
-                    <!-- single accordian area -->
-                    <div class="panel single-accordion">
-                        <h6>
-                            <a role="button" class="collapsed" aria-expanded="true" aria-controls="collapseTwo" data-parent="#accordion" data-toggle="collapse" href="#collapseTwo">VIEW PROFILE
-                                    <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
-                                    <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
-                                    </a>
-                        </h6>
-                        <div id="collapseTwo" class="accordion-content collapse">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris sceleri sque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus. Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    </div>
-                    <!-- single accordian area -->
-                    <div class="panel single-accordion">
-                        <h6>
-                            <a role="button" aria-expanded="true" aria-controls="collapseThree" class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#collapseThree">Ut ac ligula sapien. Suspendisse cursus faucibus finibus.
-                                    <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
-                                    <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
-                                </a>
-                        </h6>
-                        <div id="collapseThree" class="accordion-content collapse">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris sceleri sque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus. Lorem ipsum dolor sit amet.</p>
-                        </div>
-                </div>
+              </div>
             </div>
         </div>
-    </div>
+        <!-- single accordian area -->
+        <div class="panel single-accordion">
+            <h6>
+                <a role="button" class="collapsed" aria-expanded="true" aria-controls="collapseTwo" data-parent="#accordion" data-toggle="collapse" href="#collapseTwo">VIEW PROFILE
+                        <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                        <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
+                        </a>
+            </h6>
+            <div id="collapseTwo" class="accordion-content collapse">
+                <p>To update any of the following information complete the appropriate fields and click the "Update Profile" button.</p>
 
-
-
-       <!--  <div class="row">
-            <div class="col-12"> -->
-                <!-- Contact Form -->
-               <!--  <form method="POST">
+	          <div class="row">
+	            <div class="col-12">
+	                <!-- Contact Form -->
+	              <form method="POST">
                     <div class="row">
                     	<div class="col-lg-12">
                             <input type="text" class="form-control" name="usname" placeholder="Your Name">
@@ -165,9 +110,30 @@ if(isset($_COOKIE['login'])) {
                             <button type="submit" class="btn palatin-btn mt-50">REGISTER</button>
                         </div>
                     </div>
-                </form>
+	              </form>
+	            </div>
+	       	  </div>
             </div>
-        </div> -->
+        </div>
+            <!-- single accordian area -->
+        <div class="panel single-accordion">
+            <h6>
+                <a role="button" aria-expanded="true" aria-controls="collapseThree" class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#collapseThree">Ut ac ligula sapien. Suspendisse cursus faucibus finibus.
+                        <span class="accor-open"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                        <span class="accor-close"><i class="fa fa-minus" aria-hidden="true"></i></span>
+                    </a>
+            </h6>
+            <div id="collapseThree" class="accordion-content collapse">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris sceleri sque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus. Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+      
     
 </section>
 <!-- ##### Registration Form Area End ##### -->
