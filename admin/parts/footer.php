c78f82b97da5ac24338a4747f93f64eedc538c07
Yuliia_Sh <yu.expert@gmail.com>
2020-06-21 18:54:26 +0300

    commented out all bootstrap scripts

--- admin/parts/footer.php
+++ admin/parts/footer.php
@@ -11,25 +11,26 @@
 </div><!-- ./<div class="main-panel" id="main-panel"> -->
 
   <!--   Core JS Files   -->
-  <script src="/admin/assets/js/core/jquery.min.js"></script>
+  <!-- <script src="/admin/assets/js/core/jquery.min.js"></script> -->
   <script src="/admin/assets/js/core/popper.min.js"></script>
-  <script src="/admin/assets/js/core/bootstrap.min.js"></script>
+  <!-- <script src="/admin/assets/js/core/bootstrap.min.js"></script> -->
   <script src="/admin/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
   <!-- Chart JS -->
   <script src="/admin/assets/js/plugins/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
-  <script src="/admin/assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- <script src="/admin/assets/js/plugins/bootstrap-notify.js"></script> -->
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="/admin/assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <!-- <script src="/admin/assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script> -->
+  <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
   <script src="/admin/assets/demo/demo.js"></script>
   <script>
-    $(document).ready(function() {
+    // $(document).ready(function() {
       // Javascript method's body can be found in assets/js/demos.js
-      demo.initDashboardPageCharts();
+      // demo.initDashboardPageCharts();
 
-    });
+    // });
   </script>
 </body>
 
