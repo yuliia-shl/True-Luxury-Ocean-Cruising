d6c85561da8f8551c922bef2e5a8ab6e68b643fd
Yuliia_Sh <yu.expert@gmail.com>
2020-06-22 14:57:24 +0300

    changed styles

--- review.php
+++ review.php
@@ -1,6 +1,6 @@
-<!-- ===================================
-			Файл для отзывов
-=====================================-->
+<!-- =====================
+	Файл для отзывов
+=======================-->
 <?php
 //название раздела
 $page = "review"; 
@@ -11,7 +11,6 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
 
     // Если пользователь не авторизировался
     if( !isset($_COOKIE['login']) ){ 
-
         $user_id = 0;
 
         // Пишем запрос, чтоб проверить есть ли такой имейл в таблице users
@@ -24,7 +23,6 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
             $user_id = $user['id']; 
 			
         } else {//если пользователь не найден
-            
             $sql = "INSERT INTO users (name, email) VALUES ('". $_POST['usname'] ."', '". $_POST['usmail'] ."')";
             if ($conn->query($sql)) {
                 // Получаем id ового пользователя
@@ -33,32 +31,26 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
                echo "ERROR USER";
             }
         }
-	
-	// добовление отзыва в БД
+
+	// добавление отзыва в БД
     $sql = "INSERT INTO review (`user_id`, `date`, `rating`, `message`) 
             VALUES ('" . $user_id . "', current_timestamp(), '" . $_POST['rating'] . "', '" . $_POST['message'] . "') ";
-      
-   if ($conn->query($sql)) {
+    if ($conn->query($sql)) {
             echo "msg";
-                
         } else {
             echo "ERROR msg";
         }
-
     } else {
-        // добовление сообщение в БД
+        // добавление отзыва в БД
     $sql = "INSERT INTO review (`user_id`, `date`, `rating`, `message`) 
             VALUES ('" . $_COOKIE['login'] . "', current_timestamp(), '" . $_POST['rating'] . "', '" . $_POST['message'] . "') ";
 
         if ($conn->query($sql)) {
-
              echo "msg";
-                
         } else {
             echo "ERROR msg";
         }
     }
- 
 }
 ?>
 
@@ -70,19 +62,18 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
 </section>
 <!-- ##### Breadcumb Area End ##### -->
 
-<section class="contact-form-area mb-100 ml-100" style="text-align: center;">
-  <div class="content">
+<section class="contact-form-area mb-50 ml-100">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row justify-content-center">
+            <div class="col-8">
                 <div class="section-heading mb-1">
                     <div class="line-"></div>
                     <h2>Your Review</h2>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
+        <div class="row justify-content-center">
+            <div class="col-8">
                 <!-- Contact Form -->
                 <form method="post" action="" id="review">
                     <h3>Please leave your score!</h3>
@@ -134,9 +125,7 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
             </div>
         </div>
     </div>
-  </div>
 </section>
-<br><br><br>
 
 <?php
 
@@ -149,25 +138,28 @@ if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST") {
  $result = $conn->query($sql);
 
  while($res = mysqli_fetch_assoc($result)) { ?>
-<!--     <section class="about-us-area">
-        <div class="container">
-            <div class="row align-items-center mb-100">
-          <div class="col-12"> -->
-            <div class="reviews"> 
-                <div class="review_text">
-                     <b>Name:</b> <?= $res['name'] ?> <b >Date:</b> <?= date("d.m.y | <b></b> H.i", strtotime($res['date'])) ?> | <b>Score:</b> <?= $res['rating'] ?>/5
-                    <hr>
-                    <?= $res['message'] ?> <br>
+    <!-- ##### Testimonial Area Start ##### -->
+    <section class="testimonial-area section-padding-100 p-3 bg-img" style="background-image: url(img/core-img/pattern.png);">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <div class="testimonial-content p-0">
+                            <div class="single-testimonial">
+                                <b>Name:</b> <?= $res['name'] ?> <b >Date:</b> <?= date("d.m.y | <b></b> H.i", strtotime($res['date'])) ?> | <b>Score: <?= $res['rating'] ?>/5</b>
+                            </div> <br>
+                        <hr>
+                            <p><?= $res['message'] ?> </p>
+                    </div>
                 </div>
             </div>
-<!--           </div>
         </div>
-      </div> -->
-    <section>
+    </section>
+    <!-- ##### Testimonial Area End ##### -->
 <?php 
 } 
 ?>
 
 <?php
-// include $_SERVER['DOCUMENT_ROOT'].'/parts/footer.php';
+include $_SERVER['DOCUMENT_ROOT'].'/parts/footer.php';
 ?>
+
