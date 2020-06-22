3a56f01725f3241a069493003166f4df3a9e3a07
Viktoriia172 <vderevliuk@gmail.com>
2020-06-22 14:54:07 +0300

    modified index.php

--- index.php
+++ index.php
@@ -223,18 +223,3 @@ include $_SERVER['DOCUMENT_ROOT'].'/parts/header.php';
 include $_SERVER['DOCUMENT_ROOT'].'/parts/contact_us.php';
 include $_SERVER['DOCUMENT_ROOT'].'/parts/footer.php';
 ?>
-
-    <!-- ##### All Javascript Script ##### -->
-    <!-- jQuery-2.2.4 js -->
-    <!-- <script src="js/jquery/jquery-2.2.4.min.js"></script> -->
-    <!-- Popper js -->
-    <!-- <script src="js/bootstrap/popper.min.js"></script> -->
-    <!-- Bootstrap js -->
-    <!-- <script src="js/bootstrap/bootstrap.min.js"></script> -->
-    <!-- All Plugins js -->
-    <!-- <script src="js/plugins/plugins.js"></script> -->
-    <!-- Active js -->
-    <!-- <script src="js/active.js"></script> -->
-<!-- </body> -->
-
-<!-- </html> -->
